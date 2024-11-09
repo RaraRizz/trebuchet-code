612ccd0122b4823109d665409b33d091b790e753
RaraRizz <rahularyabollini@gmail.com>
2024-11-08 17:53:32 -0800

    Update Catapult.java

--- trebuchet-code/src/main/java/frc/robot/subsystems/Catapult.java
+++ trebuchet-code/src/main/java/frc/robot/subsystems/Catapult.java
@@ -1,11 +1,18 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Catapult extends SubsystemBase{
+    private CANSparkMax motor = new CANSparkMax(49, CANSparkMax.MotorType.kBrushless);
+
     @Override
     public void periodic(){
         System.out.println("catapult fired!");
+        motor.set(0.3);
     } 
+    
 }
+
